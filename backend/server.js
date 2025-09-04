87af381c88fbed16fdeb880ac6868ea8f24480e1
Evandro Andrade <evandro_j.o.a@hotmail.com>
2025-09-04 05:17:57 -0300

    Conectando o banco de dados

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,9 @@ app.get("/buscar-crm", async (req, res) => {
   }
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+const PORT = process.env.PORT || 5000;
+const HOST = "0.0.0.0";
+
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor rodando em http://${HOST}:${PORT}`);
 });
