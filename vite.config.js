05acd203765eb26ee8b8b8442e9fe63ac34a78e2
Evandro Andrade <evandro_j.o.a@hotmail.com>
2025-09-06 02:01:18 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ✅ caminhos relativos
+  base: './', // Caminhos relativos para evitar erros de rota
   plugins: [react()],
 })
